84dcf9b50422ff323220b16ff9c3a31b82f78706
chelz2 <golirida@hotmail.com>
2025-01-28 03:02:34 +0530

    feedback app update- events, prop drilling & react icon

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,6 @@
 import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback available</p>;
   }
@@ -8,7 +8,11 @@ function FeedbackList({ feedback }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item}></FeedbackItem>
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}
+        ></FeedbackItem>
       ))}
     </div>
   );
